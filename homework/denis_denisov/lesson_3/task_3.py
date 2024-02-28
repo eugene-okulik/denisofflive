a8c1eca045474d50be18c0bdc3e79bc5cef6c598
denis <denisofflive@mail.ru>
2024-02-28 21:25:30 +0300

    add task_1, task_2, task_3, task_4

---
+++ homework/denis_denisov/lesson_3/task_3.py
@@ -0,0 +1,13 @@
+"""
+Задача 3:
+Даны два числа. Найти среднее арифметическое и среднее геометрическое этих чисел
+"""
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+average_arithmetic = (num1 + num2) / 2
+average_geometric = (num1 * num2) ** 0.5
+
+print(f"Среднее арифметическое: {average_arithmetic}")
+print(f"Среднее геометрическое: {average_geometric}")
