3bffb72d6bb7ee2ecebaba16528de8ac8479339f
denis <denisofflive@mail.ru>
2024-03-12 14:09:11 +0300

    add tasks 1-4

---
+++ homework/denis_denisov/lesson_10/task_2.py
@@ -0,0 +1,35 @@
+"""
+Задание 2
+
+Создайте универсальный декоратор, который будет управлять тем, сколько раз запускается декорируемая функция
+
+Код, использующий этот декоратор может выглядеть, например, так:
+
+@repeat_me
+def example(text):
+    print(text)
+
+example('print me', count=2)
+В результате работы будет такое:
+
+print me
+
+print me
+
+"""
+
+
+def repeat_me(func):
+    def wrapper(*args, **kwargs):
+        count = kwargs.get('count', 1)
+        for _ in range(count):
+            func(*args)
+    return wrapper
+
+
+@repeat_me
+def example(text):
+    print(text)
+
+
+example('print me', count=2)
